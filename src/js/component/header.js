661533f081dc2e24defb154b1638f99c19c8874c
emanuelburgalin <emanuelburgalin@gmail.com>
2021-04-09 16:38:44 +0000

    terminado

---
+++ src/js/component/header.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function Header() {
+	return (
+		<div className="w-50 mb-0 mx-auto mt-5 text-center bg-dark">
+			<h1 className="display-3 text-info">To-Do-List</h1>
+		</div>
+	);
+}
